02814ab24d73854e815633cde8cd004fa37f99bf
user <user@userde-MacBook-2.local>
2017-06-19 10:20:35 +0800

    v2

--- TingKS12/BradAPI.swift
+++ TingKS12/BradAPI.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+protocol Brad01 { //只能宣告不能實作
+    func m1()
+    func m2() -> Int
+    func m3(x:Int) -> Double
+}
+protocol Brad02 {
+    func m4()
+    func m5() -> Int
+    func m6(x:Int) -> Double
+}
+class Super1 {
+    
+}
+
+class Brad03 : Super1, Brad01, Brad02{ //必須實作
+    func m1(){}
+    func m2() -> Int{return 1}
+    func m3(x:Int) -> Double{return 1.0}
+    func m4(){}
+    func m5() -> Int {return 1}
+    func m6(x:Int) -> Double {return 1.0}
+}
